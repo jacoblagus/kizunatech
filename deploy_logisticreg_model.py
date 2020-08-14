7cbe5e88007da1dbead8d795490c26c10347b581
jacoblagus <66450042+jacoblagus@users.noreply.github.com>
2020-08-14 16:18:21 -0500

    Update deploy_logisticreg_model.py

--- deploy_logisticreg_model.py
+++ deploy_logisticreg_model.py
@@ -12,13 +12,12 @@ sess = sagemaker.Session(boto3.session.Session(region_name='us-east-2'))
 role = 'arn:aws:iam::178346040475:role/service-role/AmazonSageMaker-ExecutionRole-20200616T152066'
 
 
-request = requests.get('https://0n5dn59xeb.execute-api.us-east-2.amazonaws.com/LogisticStart')
-print(request.status_code)
+# Use this code if api key is required for lambda function
+#customHeader = {'x-api-key': '6NN6vAjuJRqENqcXccJF1DdOqUBpw2f6RitcO3af'}
+#r = requests.post('https://jert93zwv9.execute-api.us-east-2.amazonaws.com/FirstStage/test-logistic', data=json.dumps({"data":"1.0000000e+00, 2.3180000e+01, 2.7271999e+01, 4.2600000e+02,7.2125000e+02, 4.7929883e-03, 1.0000000e+00"}), headers=customHeader)
+#r.text
 
 
-customHeader = {'x-api-key': '6NN6vAjuJRqENqcXccJF1DdOqUBpw2f6RitcO3af'}
-r = requests.post('https://jert93zwv9.execute-api.us-east-2.amazonaws.com/FirstStage/test-logistic', data=json.dumps({"data":"1.0000000e+00, 2.3180000e+01, 2.7271999e+01, 4.2600000e+02,7.2125000e+02, 4.7929883e-03, 1.0000000e+00"}), headers=customHeader)
-
-
-
-r.text
+# Use this code when api key is not needed.
+p = requests.post('https://jert93zwv9.execute-api.us-east-2.amazonaws.com/FirstStage/test-logistic', data=json.dumps({"data":"1.0000000e+00, 2.3180000e+01, 2.7271999e+01, 4.2600000e+02,7.2125000e+02, 4.7929883e-03, 1.0000000e+00"}))
+p.text
